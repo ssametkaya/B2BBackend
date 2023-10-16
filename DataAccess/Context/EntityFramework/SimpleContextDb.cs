2a801c5344e98af525c8dfd0e885699ec059b20c
Süleyman Samet Kaya <ssametkaya52@gmail.com>
2023-10-16 15:28:51 +0300

    Initial commit

---
+++ DataAccess/Context/EntityFramework/SimpleContextDb.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context.EntityFramework
+{
+    public class SimpleContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-94U96R1;Database=B2BDb;Integrated Security=true;");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<EmailParameter> EmailParameters { get; set; }
+    }
+}
